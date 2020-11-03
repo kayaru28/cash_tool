bbdaf28bd66f5135ced383afa9a74f963cb12c85
kayaru28 <istorytale090415@gmail.com>
2020-11-03 10:38:49 +0900

    refuctoring and making id randomize

--- 02_flask001/mysql_proc.py
+++ 02_flask001/mysql_proc.py
@@ -2,9 +2,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import random
 
+root_user = "root"
+root_pass = "Root-0904"
+root_host = "localhost"
+db_name_rps = "rps"
 
-engine=create_engine("mysql://root:Root-0904@localhost/rps")
+engine_key_rps = "mysql://"+root_user+":"+root_pass+"@"+root_host+"/" + db_name_rps
+
+engine=create_engine(engine_key_rps)
 
 SessionClass=sessionmaker(engine)
 session=SessionClass()
@@ -15,8 +22,8 @@ class BattleHistory(Base):
     __table_args__={"autoload": True}
 
 
-
-insert_battle_history=BattleHistory(id=1,time="2018-04-10 12:00:00", name="test", choice_id=20,result="win")
+tmpid = int(random.random()*1000000000)
+insert_battle_history=BattleHistory(id=tmpid,time="2018-04-10 12:00:00", name="test", choice_id=20,result="win")
 session.add(insert_battle_history)
 session.commit()
 
