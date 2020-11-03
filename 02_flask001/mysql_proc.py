f552c08516c07e4a2602f98dca5f5f490a446084
kayaru28 <istorytale090415@gmail.com>
2020-11-03 10:45:54 +0900

    refuctoring

--- 02_flask001/mysql_proc.py
+++ 02_flask001/mysql_proc.py
@@ -9,24 +9,35 @@ root_pass = "Root-0904"
 root_host = "localhost"
 db_name_rps = "rps"
 
-engine_key_rps = "mysql://"+root_user+":"+root_pass+"@"+root_host+"/" + db_name_rps
-
-engine=create_engine(engine_key_rps)
-
-SessionClass=sessionmaker(engine)
-session=SessionClass()
-
-Base=declarative_base(bind=engine)
-class BattleHistory(Base):
-    __tablename__="battle_history" 
-    __table_args__={"autoload": True}
 
 
-tmpid = int(random.random()*1000000000)
-insert_battle_history=BattleHistory(id=tmpid,time="2018-04-10 12:00:00", name="test", choice_id=20,result="win")
-session.add(insert_battle_history)
-session.commit()
+def getEngineKey(db_name):
+    return "mysql://"+root_user+":"+root_pass+"@"+root_host+"/" + db_name
 
-session.close()
+def getRandomId9():
+    return int(random.random()*1000000000)
+
+
+def rps_insert():
+
+    engine_key = getEngineKey(db_name_rps)
+    engine=create_engine(engine_key)
+    Base=declarative_base(bind=engine)
+    class BattleHistory(Base):
+        __tablename__="battle_history" 
+        __table_args__={"autoload": True}
+
+    SessionClass=sessionmaker(engine)
+    session=SessionClass()
+
+    tmpid = getRandomId9()
+    insert_battle_history=BattleHistory(id=tmpid,time="2018-04-10 12:00:00", name="test", choice_id=20,result="win")
+
+    session.add(insert_battle_history)
+    session.commit()
+
+    session.close()
+
+rps_insert()
 
 
